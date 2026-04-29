c316bba2c673b5f6ccf446460167d2655aad253a
fredysan651 <fredyestupinan10@gmail.com>
2026-04-28 19:44:21 -0500

    Fix: corregir path del import de environment (3 niveles no 4)

--- src/app/pages/perfil-estudiante/perfil-estudiante.component.ts
+++ src/app/pages/perfil-estudiante/perfil-estudiante.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { CommonModule } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import { EstudiantesService } from '../../services/estudiantes.service';
